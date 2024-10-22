fc4f978c9756840d9e6ce2e70196a26eaf428957
Your Name <you@example.com>
2024-10-22 19:34:50 +0300

    done

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,23 @@ from datetime import datetime
 class BaseModel:
     """A base class model that provides common functionality for all models."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This initializes instance attribute."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    if key != "__class__":
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the instance."""
